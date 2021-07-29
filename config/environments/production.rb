7d6dc408cb0fb3e84ab32e9e3410f56938f22124
Bluette1 <marylene.sawyer@gmail.com>
2021-07-29 17:48:39 +0200

    Allow Heroku hostname

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -112,5 +112,6 @@ Rails.application.configure do
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 
   # Allow requests for all hostnames
-  config.hosts.clear
+  # config.hosts.clear
+  config.hosts << "thawing-thicket-49646.herokuapp.com"
 end
