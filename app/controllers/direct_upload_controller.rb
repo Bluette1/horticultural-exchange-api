d6ea10cfb41f70c3e0ce47f9e79dbce222ae32f2
Bluette1 <marylene.sawyer@gmail.com>
2021-08-10 05:30:00 +0200

    Finish the presign ul mechanism

--- app/controllers/direct_upload_controller.rb
+++ app/controllers/direct_upload_controller.rb
@@ -1,9 +1,12 @@
 class DirectUploadController < ApplicationController
-  response = generate_direct_upload(blob_params)
+  # before_action :authenticate_user!
+def create
+    response = generate_direct_upload(blob_params)
   render json: response
 end
 
-private
+
+  private
 def blob_params
   params.require(:file).permit(:filename, :byte_size, :checksum, :content_type, metadata: {})
 end
@@ -14,3 +17,28 @@ def generate_direct_upload(blob_args)
   response[:blob_signed_id] = blob.signed_id
   response
 end
+def create_blob(blob_args)
+  blob = ActiveStorage::Blob.create_before_direct_upload!(blob_args.to_h.deep_symbolize_keys)
+  pdf_id = SecureRandom.uuid # the name of the file will just be a UUID
+  blob.update_attribute(:key, "uploads/#{pdf_id}") # will put it in the uploads folder
+  blob
+end
+
+def signed_url(blob)
+  expiration_time = 10.minutes
+  response_signature(
+    blob.service_url_for_direct_upload(expires_in: expiration_time),
+    headers: blob.service_headers_for_direct_upload
+  )
+end
+
+def response_signature(url, **params)
+  {
+    direct_upload: {
+      url: url
+    }.merge(params)
+  }
+end
+end
+
+
