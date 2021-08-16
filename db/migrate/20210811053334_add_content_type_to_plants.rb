ba5e68221af9f1cc086432371ceea03bcdc45b92
Bluette1 <marylene.sawyer@gmail.com>
2021-08-16 05:34:58 +0200

    Rollback the last migrations

--- db/migrate/20210811053334_add_content_type_to_plants.rb
+++
@@ -1,5 +0,0 @@
-class AddContentTypeToPlants < ActiveRecord::Migration[6.1]
-  def change
-    add_column :plants, :content_type, :string
-  end
-end
--- db/migrate/20210811053420_add_file_name_to_plants.rb
+++
@@ -1,5 +0,0 @@
-class AddFileNameToPlants < ActiveRecord::Migration[6.1]
-  def change
-    add_column :plants, :filename, :string
-  end
-end
