5af642b3a28c5cdebbfc94b7b1d49a6df1d50f61
Bluette1 <marylene.sawyer@gmail.com>
2021-10-04 11:02:29 +0200

    Fix failing ci
    issue #2

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -1,7 +1,4 @@
 require "active_support/core_ext/integer/time"
-require 'dotenv'
-
-Dotenv.load('.env.test')
 
 # The test environment is used exclusively to run your application's
 # test suite. You never need to work with it otherwise. Remember that
