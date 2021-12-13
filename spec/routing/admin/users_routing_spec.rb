0fd1d9310dcb397285964884658f3b229d4cf746
Bluette1 <marylene.sawyer@gmail.com>
2021-12-13 04:53:30 +0200

    Fix lint errors

--- spec/routing/admin/users_routing_spec.rb
+++ spec/routing/admin/users_routing_spec.rb
@@ -1,17 +1,17 @@
-require "rails_helper"
+require 'rails_helper'
 
 RSpec.describe Admin::UsersController, type: :routing do
-  describe "routing" do
-    it "routes to #index" do
-      expect(get: "/admin/users").to route_to("admin/users#index")
+  describe 'routing' do
+    it 'routes to #index' do
+      expect(get: '/admin/users').to route_to('admin/users#index')
     end
 
-    it "routes to #create" do
-      expect(post: "/admin/users").to route_to("admin/users#create")
+    it 'routes to #create' do
+      expect(post: '/admin/users').to route_to('admin/users#create')
     end
 
-    it "routes to #destroy" do
-      expect(delete: "/admin/users/1").to route_to("admin/users#destroy", id: "1")
+    it 'routes to #destroy' do
+      expect(delete: '/admin/users/1').to route_to('admin/users#destroy', id: '1')
     end
   end
 end
--- spec/routing/api/wishes_routing_spec.rb
+++ spec/routing/api/wishes_routing_spec.rb
@@ -1,17 +1,17 @@
-require "rails_helper"
+require 'rails_helper'
 
 RSpec.describe Api::WishesController, type: :routing do
-  describe "routing" do
-    it "routes to #index" do
-      expect(get: "/api/wishes").to route_to("api/wishes#index", format: :json)
+  describe 'routing' do
+    it 'routes to #index' do
+      expect(get: '/api/wishes').to route_to('api/wishes#index', format: :json)
     end
 
-    it "routes to #create" do
-      expect(post: "/api/plants/1/wishes").to route_to("api/wishes#create", :plant_id=>"1", format: :json)
+    it 'routes to #create' do
+      expect(post: '/api/plants/1/wishes').to route_to('api/wishes#create', plant_id: '1', format: :json)
     end
 
-    it "routes to #destroy" do
-      expect(delete: "/api/wishes/1").to route_to("api/wishes#destroy", id: "1", format: :json)
+    it 'routes to #destroy' do
+      expect(delete: '/api/wishes/1').to route_to('api/wishes#destroy', id: '1', format: :json)
     end
   end
 end
