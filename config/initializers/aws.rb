f7318e3d15c22d2b88aa1dcc9000af984e7fe742
Bluette1 <marylene.sawyer@gmail.com>
2024-10-15 07:48:41 +0200

    fix: Precompiled assets
    - Add Precompiled assets for Heroku Deployment

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -1,8 +1,9 @@
 require 'aws-sdk-s3'
 
 Aws.config.update({
-  region: ENV['AWS_REGION'],
-  credentials: Aws::Credentials.new(ENV['AWS_ACCESS_KEY_ID'], ENV['AWS_SECRET_ACCESS_KEY']),
-})
+                    region: ENV.fetch('AWS_REGION', nil),
+                    credentials: Aws::Credentials.new(ENV.fetch('AWS_ACCESS_KEY_ID', nil),
+                                                      ENV.fetch('AWS_SECRET_ACCESS_KEY', nil))
+                  })
 
-S3_BUCKET = Aws::S3::Resource.new.bucket(ENV['S3_BUCKET'])
+S3_BUCKET = Aws::S3::Resource.new.bucket(ENV.fetch('S3_BUCKET', nil))
